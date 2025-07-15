26d5bf1abdbda1e1bf04b4455aaf96c3e058d812
takubot83 <live.to.ride.ride.to.live060811@gmail.com>
2025-07-15 22:38:03 +0900

    Fix Vercel deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
